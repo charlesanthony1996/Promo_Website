0ea719c53bd509913dd4e3137a0a51f51ec59be5
Charles Anthony <charlesanthony296@gmail.com>
2024-05-27 14:46:53 +0200

    commented out about.css

--- src/components/About.vue
+++ src/components/About.vue
@@ -141,7 +141,7 @@ export default {
 </script>
 
 <style scoped>
-@import "../styles/about.css";
+/* @import "../styles/about.css"; */
 @import "../styles/output.css";
 @import "../styles/custom.css";
 </style>
